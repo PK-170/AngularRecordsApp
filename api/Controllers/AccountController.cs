f6b6cd9a24548f21b3c0d51b9ae0bf88a0ade99a
PK-170 <pawan59c@gmail.com>
2024-10-21 22:02:06 +1100

    adding interface

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 //using api.Dtos.Account;
-//using api.Interfaces;
+using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
