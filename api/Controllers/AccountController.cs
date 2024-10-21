02a53a594fb59b6eeeee71f55faee574fb871244
PK-170 <pawan59c@gmail.com>
2024-10-21 22:00:13 +1100

    adding model

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -2,7 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+//using api.Dtos.Account;
+//using api.Interfaces;
+using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -10,6 +15,12 @@ namespace api.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _tokenService;
+
+        private readonly SignInManager<AppUser> _signinManager;
+
 
     }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45fa3f3984378dd8f2df76328482168342863078")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cd8f8543e39edb0825cf1cf76cbccd8b06f8c22")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
