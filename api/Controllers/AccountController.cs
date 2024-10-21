4cd8f8543e39edb0825cf1cf76cbccd8b06f8c22
PK-170 <pawan59c@gmail.com>
2024-10-21 21:55:17 +1100

    adding controller

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
-    public class AccountController
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
     {
 
     }
