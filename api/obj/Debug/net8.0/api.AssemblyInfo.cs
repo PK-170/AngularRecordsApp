93e6c8ec8c1560fcdf6dff59d527cf32a0e0a46b
PK-170 <pawan59c@gmail.com>
2024-10-22 22:09:51 +1100

    adding register dto

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b4f8b2f22281d0e8d1cf07b29ad5b8ad5be98c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fdb057a945d7b36a53b284ff23ce7f73fbded6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
