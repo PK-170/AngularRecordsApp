b086c86f32bde43635892fd68de42af87e15256b
PK-170 <pawan59c@gmail.com>
2024-10-22 22:20:42 +1100

    adding new user dto

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,6 +15,39 @@ namespace api.Data
         public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
         {
 
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+
+                },
+                new IdentityRole
+                {
+                    Name = "Manager",
+                    NormalizedName = "MANAGER"
+                },
+                new IdentityRole
+                {
+                    Name = "Engineer",
+                    NormalizedName = "ENGINEER"
+                },
+                new IdentityRole
+                {
+                    Name = "Foreman",
+                    NormalizedName = "FOREMAN"
+                },
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
 
     }
