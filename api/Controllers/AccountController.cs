9e3e360f4a4d3c079e7a1f2345b1a0c5c41adb09
PK-170 <pawan59c@gmail.com>
2024-10-23 20:03:50 +1100

    adding new user dto

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -16,7 +16,7 @@ namespace api.Controllers
 
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
-        
+
 
         private readonly SignInManager<AppUser> _signinManager;
 
@@ -27,33 +27,7 @@ namespace api.Controllers
             _signinManager = signInManager;
         }
 
-        [HttpPost("login")]
 
-        public async Task<IActionResult> Login(LoginDto loginDto)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            
-
-            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.UserName);
-
-            if (user == null) return Unauthorized("Invalid Username!");
-
-            var result = await _signinManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
-
-            if (!result.Succeeded) return BadRequest("UserName not found and/or password is incorrect");
-
-            return Ok(
-             new Dtos.NewUserDto
-             {
-                 UserName = user.UserName,
-                 Email = user.Email,
-                 Token = _tokenService.CreateToken(user)
-             }
-            );
-        }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] Dtos.RegisterDto registerDto)
