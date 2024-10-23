c15f7e94b6176c4e0e966413a3d6dd5b9e2fc13f
PK-170 <pawan59c@gmail.com>
2024-10-23 20:02:56 +1100

    adding new user dto

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb7d040ae85d8b1130455bdaf72d55a7905b39bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9fc56ceeea557add9376fd37941fb7ee696e3fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
