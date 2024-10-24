229662b2c4b80e02223e89de4ed3c089ec6491d6
PK-170 <pawan59c@gmail.com>
2024-10-24 22:24:56 +1100

    added files

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
     public class ApplicationDBContext : IdentityDbContext<AppUser>
     {
+
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+         : base(dbContextOptions)
+        {
+
+        }
 
     }
 }
