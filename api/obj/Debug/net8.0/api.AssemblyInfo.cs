be195a5045d0e525d67d448eeea53c115b8c6ab3
PK-170 <pawan59c@gmail.com>
2024-10-24 22:18:41 +1100

    added files

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f877818c06cc6fb6acbd5d1801069d6a2076198c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+958bbee19ac74a4d8197f5413dec94fc017b202f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
