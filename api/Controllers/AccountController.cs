e54a59c1958405592c9c53d3ba3eea581c92629e
pawan kumar <pawan59c@gmail.com>
2024-10-27 21:41:56 +1100

    added controller

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -10,6 +12,12 @@ namespace api.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
         
     }
 }
