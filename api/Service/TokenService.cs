2427b7fc1037299697160ea404710077f7197a7f
pawan kumar <pawan59c@gmail.com>
2024-10-29 20:22:21 +1100

    added files

--- api/Service/TokenService.cs
+++ api/Service/TokenService.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using api.Interfaces;
@@ -19,6 +21,15 @@ namespace api.Service
             _config = config;
             _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:SigningKey"]));
         }
+
+         public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+               new Claim(JwtRegisteredClaimNames.Email, user.Email),
+               new Claim(JwtRegisteredClaimNames.GivenName, user.UserName)
+            };
+            var crads = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512);
        
        
         
