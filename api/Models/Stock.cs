e1cbc4a10fcf4deacc71352cd60439ae23c6a2a9
pawan kumar <pawan59c@gmail.com>
2024-10-31 22:33:58 +1100

    added files

--- api/Models/Stock.cs
+++ api/Models/Stock.cs
@@ -21,7 +21,7 @@ namespace api.Models
 
         public string Industry { get; set; } = string.Empty;
         public long MarketCap { get; set; }
-        //public List<Comment> comments { get; set; } = new List<Comment>();
+        
         
     }
 }
