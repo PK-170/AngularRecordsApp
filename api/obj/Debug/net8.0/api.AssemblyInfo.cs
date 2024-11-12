aa4d59a0c04cd77f9757ef1f0cea66185c5a451b
PK-170 <pawan59c@gmail.com>
2024-11-12 22:55:18 +1100

    added controller

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a3766b5a55ab6e89938413844b69c70b146159e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c87069947dc6d217eda01b3d5bb9c0401da639d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
