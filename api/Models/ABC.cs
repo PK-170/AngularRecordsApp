5dedf5bcb4b60d0fe5edb7cc98f45d6ea9f3d6cb
PK-170 <pawan59c@gmail.com>
2024-11-14 21:33:03 +1100

    added field

--- api/Models/ABC.cs
+++ api/Models/ABC.cs
@@ -8,7 +8,7 @@ namespace api.Models
     public class ABC
     {
         public int Id { get; set; }
-        public required DateTime Date { get; set; }
+        public DateTime Date { get; set; }
         public string? Email { get; set; } = string.Empty;
 
         public required string Phone { get; set; } = string.Empty;
