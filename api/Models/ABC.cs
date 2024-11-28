42e2da12666c3ddcee8fcfeff9d5eee2e12e7e52
PK-170 <pawan59c@gmail.com>
2024-11-28 21:28:16 +1100

    added files

--- api/Models/ABC.cs
+++ api/Models/ABC.cs
@@ -9,7 +9,7 @@ namespace api.Models
     {
         public int Id { get; set; }
         
-       
+        public DateTime Date { get; set; }
 
         public string Type { get; set; } = string.Empty;
 
