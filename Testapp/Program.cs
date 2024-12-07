dab40b90e42998da4a2ca147a5ebe32d9674dc63
PK-170 <pawan59c@gmail.com>
2024-12-07 11:44:38 +1100

    added files

--- Testapp/Program.cs
+++ Testapp/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!!");
+
+string bill = "This is a test";
+int num = 1;
+var a = "This is var test";
+
+Console.WriteLine(bill);
+Console.WriteLine(num);
+Console.WriteLine(a);
