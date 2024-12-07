f642a82f859bae71fc4fcb757c604d1b264508f8
PK-170 <pawan59c@gmail.com>
2024-12-07 12:37:53 +1100

    added files

--- Testapp/Program.cs
+++ Testapp/Program.cs
@@ -1,10 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Text;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!!");
 
 string bill = "This is a test";
 int num = 1;
 var a = "This is var test";
+//var b = 'a';
+//a[2] = 'd';
 
-Console.WriteLine(bill);
+Console.WriteLine($"This is template litral {bill} ");
 Console.WriteLine(num);
 Console.WriteLine(a);
+
+StringBuilder stringBuilder = new StringBuilder();
+
+stringBuilder.Append("Crab is good");
+
+
+Console.WriteLine(stringBuilder);
