1181d3f4afec740a1f72ca9a059040ba48a05c7e
PK-170 <pawan59c@gmail.com>
2024-12-11 18:17:27 +1100

    added while  statement

--- Testapp/Program.cs
+++ Testapp/Program.cs
@@ -8,7 +8,10 @@ PingReply PR =  p1.Send("8.8.8.8");
 while(PR.Status.ToString() == "Success")
 {
    Console.WriteLine(PR.Status.ToString());
-   
+   PR =  p1.Send("8.8.8.8");
+   Console.WriteLine("Wait ten seconds for new request");
+   Thread.Sleep(1000);
+
 }
 
 
