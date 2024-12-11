e6ba557f5ebfd97931990f3938fe76fa01bd9a10
PK-170 <pawan59c@gmail.com>
2024-12-11 18:07:38 +1100

    added while  statement

--- Testapp/Program.cs
+++ Testapp/Program.cs
@@ -1,76 +1,86 @@
 ﻿using System;
 using System.Text;
+using System.Net.NetworkInformation;
+
+Ping p1 = new Ping();
+
+
+
+
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!");
+// Console.WriteLine("Hello, World!!");
 
-string bill = "This is a test";
-int num = 1;
-var a = "This is var test";
-//var b = 'a';
-//a[2] = 'd';
+// string bill = "This is a test";
+// int num = 1;
+// var a = "This is var test";
+// //var b = 'a';
+// //a[2] = 'd';
 
-Console.WriteLine($"This is template litral {bill} ");
-Console.WriteLine(num);
-Console.WriteLine(a);
+// Console.WriteLine($"This is template litral {bill} ");
+// Console.WriteLine(num);
+// Console.WriteLine(a);
 
-StringBuilder stringBuilder = new StringBuilder();
+// StringBuilder stringBuilder = new StringBuilder();
 
-stringBuilder.Append("Crab is good");
-stringBuilder.Remove(0,3);
-
-
-Console.WriteLine(stringBuilder);
-
-string[] arr = ["apple", "Banana", "Carrot"];
-foreach(var item in arr)
-{
-    Console.WriteLine(item);
-}
-
-for(var i=0; i<arr.Length; i++)
-{
-    Console.WriteLine(arr[i]);
-}
-
-arr.ToList().ForEach((x)=> 
-{Console.WriteLine(x);
-});
-
-Array.ForEach(arr, x=> Console.WriteLine(x));
-
-var FishTank = 100;
-if(FishTank <= 100)
-{
-    Console.WriteLine("I'll buy fish");
-}
-else
-{
-    Console.WriteLine("I'll skip");
-}
-
-var isString = "Fish is a string";
-if(isString is string)
-{
-    Console.WriteLine("Its a string for sure");
-}
+// stringBuilder.Append("Crab is good");
+// stringBuilder.Remove(0,3);
 
 
-Console.WriteLine((int)Warning.CodeBlue);
-enum Warning
-{
-    CodeRed,
-    CodeBlue,
-    CodeYellow
-}
+// Console.WriteLine(stringBuilder);
 
-var name = "Ming";
+// string[] arr = ["apple", "Banana", "Carrot"];
+// foreach(var item in arr)
+// {
+//     Console.WriteLine(item);
+// }
 
-var result = name switch 
-{
-    "Zihan" => "Zihan",
-    "Ming" => "Ming",
-    _ => "nothing match",
-};
+// for(var i=0; i<arr.Length; i++)
+// {
+//     Console.WriteLine(arr[i]);
+// }
 
-Console.WriteLine( "This is from switch" + result);
+// arr.ToList().ForEach((x)=> 
+// {Console.WriteLine(x);
+// });
+
+// Array.ForEach(arr, x=> Console.WriteLine(x));
+
+// var FishTank = 100;
+// if(FishTank <= 100)
+// {
+//     Console.WriteLine("I'll buy fish");
+// }
+// else
+// {
+//     Console.WriteLine("I'll skip");
+// }
+
+// var isString = "Fish is a string";
+// if(isString is string)
+// {
+//     Console.WriteLine("Its a string for sure");
+// }
+
+
+// // Console.WriteLine((int)Warning.CodeBlue);
+// // enum Warning
+// // {
+// //     CodeRed,
+// //     CodeBlue,
+// //     CodeYellow
+// // }
+
+
+
+// var name = "Ming";
+
+// var result = name switch 
+// {
+//     "Zihan" => "Zihan",
+//     "Ming" => "Ming",
+//     _ => "nothing match"
+// };
+
+// Console.WriteLine( "This is from switch" + result);
+
