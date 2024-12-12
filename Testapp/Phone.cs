d254ed5e9e2b68cf3a1049abdabb58ff6ea40813
PK-170 <pawan59c@gmail.com>
2024-12-12 23:26:53 +1100

    added class

---
+++ Testapp/Phone.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testapp
+{
+    public class Phone
+    {
+        public string Brand { get; set; }
+    }
+}
